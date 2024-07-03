a9956c73dfccfb3f5439644995b0ece4c741bc4b
MauricioNoriegaTec <mau.noriega10@gmail.com>
2024-07-03 00:53:23 -0600

    Recursion

---
+++ Recursion/HowRecursionWorks/Code/recursiveFunctions.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int fun(int x) {
+    if (x > 0) {
+        cout << x << endl;
+        fun(x-1);
+    }
+    return 0;
+}
+
+int fun2(int x) {
+    if (x > 0) {
+        fun2(x-1);
+        cout << x << endl;
+    }
+    return 0;
+}
+
+int main() {
+    int x = 3;
+    fun(x);
+    cout << endl;
+    fun2(x);
+}
